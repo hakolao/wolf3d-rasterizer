d7d32516d33336195f8fbd9893d2a10776c01aa7
hakolao <okkohakola@gmail.com>
2020-09-08 17:28:45 +0300

    Fix crucial iteration bug

--- lib/libgmatrix/ml_matrix4_mul_vec.c
+++ lib/libgmatrix/ml_matrix4_mul_vec.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/16 17:24:58 by ohakola           #+#    #+#             */
-/*   Updated: 2020/08/25 17:18:42 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/09/08 17:28:07 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,20 @@ void		ml_matrix4_mul_vec3(t_mat4 m, t_vec3 v, t_vec3 res)
 	size_t		c;
 	size_t		i;
 	t_vec4		v4;
+	t_vec4		res4;
 
-	ft_memset(res, 0, sizeof(t_vec3));
+	ft_memset(res4, 0, sizeof(t_vec4));
 	ml_vector3_to_vec4(v, v4);
 	c = -1;
 	while (++c < 4)
 	{
 		r = -1;
 		while (++r < 4)
-			res[c] += m[r][c] * v4[r];
+			res4[c] += m[r][c] * v4[r];
 	}
 	i = -1;
 	while (++i < 3)
-		v[i] = v4[i];
+		res[i] = res4[i];
 }
 
 void		ml_matrix4_mul_vec4(t_mat4 m, t_vec4 v, t_vec4 res)

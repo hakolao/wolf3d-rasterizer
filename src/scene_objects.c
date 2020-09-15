598bddc7d06a01f7e396f667a493f59b4b1eb4c2
Vesa Eilo <veilo@c2r6p1.hive.fi>
2020-09-15 14:00:02 +0300

    got triangle on screen

--- src/scene_objects.c
+++ src/scene_objects.c
@@ -45,33 +45,3 @@ void				scene_vertices_init(t_wolf3d *app, t_scene *scene)
 		}
 	}
 }
-
-/*
-** Add assets as .obj files to scenes
-*/
-
-t_3d_object		**create_scene_test_objects(int32_t *object_count)
-{
-	t_3d_object **objects;
-	*object_count = 1;
-	error_check(!(objects = malloc(sizeof(*objects) * *object_count)),
-				"Failed to malloc objects for test scene");
-	objects[0] = create_3d_object();
-	error_check(!(objects[0]->mesh_vertices = malloc(sizeof(t_vertex) * 3)),
-				"Failed to malloc vertices for test scene object");
-	objects[0]->mesh_vertex_count = 3;
-	ft_memset(&objects[0]->mesh_vertices[0], 0, sizeof(t_vertex));
-	ft_memset(&objects[0]->mesh_vertices[1], 0, sizeof(t_vertex));
-	ft_memset(&objects[0]->mesh_vertices[2], 0, sizeof(t_vertex));
-	ml_vector3_copy((t_vec3){WIDTH, 20.0, 40.0}, objects[0]->mesh_vertices[0]->position);
-	ml_vector3_copy((t_vec3){WIDTH, 30.0, -40.0}, objects[0]->mesh_vertices[1]->position);
-	ml_vector3_copy((t_vec3){WIDTH, -50.0, -50.0}, objects[0]->mesh_vertices[2]->position);
-	error_check(!(objects[0]->mesh_triangles = malloc(sizeof(t_triangle) * 1)),
-				"Failed to malloc triangles for test scene object");
-	objects[0]->mesh_triangle_count = 1;
-	init_triangle(&objects[0]->mesh_triangles[0],
-				  &objects[0]->mesh_vertices[0],
-				  &objects[0]->mesh_vertices[1],
-				  &objects[0]->mesh_vertices[2]);
-	return (objects);
-}

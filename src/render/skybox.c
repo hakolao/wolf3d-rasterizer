157185958ae9eeb462f93dd70fc2fbb8888f29fe
Okko Hakola <ohakola@c2r4p1.hive.fi>
2020-11-24 17:58:00 +0200

    Add a comment

--- src/render/skybox.c
+++ src/render/skybox.c
@@ -6,11 +6,15 @@
 /*   By: ohakola+veilo <ohakola+veilo@student.hi    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 17:46:05 by ohakola+vei       #+#    #+#             */
-/*   Updated: 2020/11/24 17:47:09 by ohakola+vei      ###   ########.fr       */
+/*   Updated: 2020/11/24 17:57:47 by ohakola+vei      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
+
+/*
+** Skybox is rotated, but it is not translated unlike other objects in scene
+*/
 
 static void		set_skybox_render_triangle(t_wolf3d *app,
 									t_triangle *r_triangle,

7df82e38f632efdef34ba6e14d050f8eee20a39d
Vesa Eilo <veilo@c2r4p3.hive.fi>
2020-11-29 19:54:21 +0200

    minor buff

--- lib/lib3d/l3d_triangle_raster.c
+++ lib/lib3d/l3d_triangle_raster.c
@@ -70,10 +70,10 @@ void		normal_from_color(uint32_t color, t_vec3 normal)
 	uint32_t	rgba[4];
 
 	l3d_u32_to_rgba(color, rgba);
-	normal[0] = (float)rgba[0] / 255;
-	normal[1] = (float)rgba[1] / 255;
-	normal[2] = (float)rgba[2] / 255;
-	
+	normal[0] = (float)rgba[0] * 0.00392156862745098;
+	normal[1] = (float)rgba[1] * 0.00392156862745098;
+	normal[2] = (float)rgba[2] * 0.00392156862745098;
+
 	(void)normal;
 	(void)color;
 	(void)rgba;
@@ -94,6 +94,9 @@ void		calc_bumped_normal(t_triangle *triangle, t_vec2 uv, t_vec3 res)
 						triangle->normalized_normal, tbn);
 	ml_matrix3_mul_vec3(tbn, bumpnormal, resultnormal);
 	ml_vector3_normalize(resultnormal, res);
+	(void)triangle;
+	(void)uv;
+	(void)res;
 }
 
 uint32_t		fragment_shade_normal(t_vec3 light_vector, t_vec3 frag_normal,
@@ -114,7 +117,7 @@ uint32_t		fragment_shade_normal(t_vec3 light_vector, t_vec3 frag_normal,
 	return (l3d_rgba_to_u32(rgba));
 }
 
-static uint32_t pixel_normal_shaded(uint32_t pixel, t_triangle *triangle, t_vec2 uv)
+uint32_t pixel_normal_shaded(uint32_t pixel, t_triangle *triangle, t_vec2 uv)
 {
 	t_vec3 frag_normal;
 	t_vec3 light_vector;
@@ -122,6 +125,8 @@ static uint32_t pixel_normal_shaded(uint32_t pixel, t_triangle *triangle, t_vec2
 	ml_vector3_set(light_vector, 0.0, 0.0, -1.0);
 	ml_vector3_set_all(frag_normal, 0.0);
 	calc_bumped_normal(triangle, uv, frag_normal);
+	(void)triangle;
+	(void)uv;
 	return (fragment_shade_normal(light_vector, frag_normal, pixel));
 }
 
@@ -129,7 +134,7 @@ static uint32_t	pixel_depth_shaded(uint32_t pixel, float z_val)
 {
 	float	intensity;
 
-	intensity = 10.0;
+	intensity = 5.0;
 	return (l3d_color_blend_u32(pixel, 0x000000ff,
 		1.0 - (ft_abs(z_val) * intensity)));
 }

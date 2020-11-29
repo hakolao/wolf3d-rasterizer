abf4ca254bcd54829c1343ef35111f7f1360d343
hakolao <okkohakola@gmail.com>
2020-11-30 01:47:09 +0200

    Fix segfault

--- src/events/mouse.c
+++ src/events/mouse.c
@@ -6,7 +6,7 @@
 /*   By: ohakola+veilo <ohakola+veilo@student.hi    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 14:35:00 by ohakola           #+#    #+#             */
-/*   Updated: 2020/11/29 16:20:16 by ohakola+vei      ###   ########.fr       */
+/*   Updated: 2020/11/30 01:44:35 by ohakola+vei      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ void					mouse_state_set(t_wolf3d *app)
 
 void					mouse_state_handle(t_wolf3d *app)
 {
+	if (app->active_scene->scene_id != scene_id_main_game)
+		return ;
 	if (!app->player.is_shooting && (app->mouse.state & SDL_BUTTON_LMASK))
 	{
 		app->player.is_shooting = true;

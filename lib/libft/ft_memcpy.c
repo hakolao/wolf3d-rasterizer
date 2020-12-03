39ac4a9609c452ad4e1fce9cb3deefc5776d2501
hakolao <okkohakola@gmail.com>
2020-12-04 00:04:17 +0200

    Fix memset and memcopy to be more compiler optimized again

--- lib/libft/ft_memcpy.c
+++ lib/libft/ft_memcpy.c
@@ -3,41 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: ohakola+veilo <ohakola+veilo@student.hi    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 13:33:56 by ohakola           #+#    #+#             */
-/*   Updated: 2020/10/19 15:31:29 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/12/03 23:52:39 by ohakola+vei      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_utils.h"
-
-static void		copy_by_3(char *destcpy, const char *srccpy, size_t n)
-{
-	size_t		i;
-
-	i = 0;
-	while (i < n)
-	{
-		destcpy[i] = srccpy[i];
-		destcpy[i + 1] = srccpy[i + 1];
-		destcpy[i + 2] = srccpy[i + 2];
-		i += 3;
-	}
-}
-
-static void		copy_by_2(char *destcpy, const char *srccpy, size_t n)
-{
-	size_t		i;
-
-	i = 0;
-	while (i < n)
-	{
-		destcpy[i] = srccpy[i];
-		destcpy[i + 1] = srccpy[i + 1];
-		i += 2;
-	}
-}
 
 /*
 ** The ft_memcpy() function copies n bytes from memory area src to memory
@@ -56,22 +29,7 @@ void			*ft_memcpy(void *dst, const void *src, size_t n)
 		return (NULL);
 	destcpy = dst;
 	srccpy = src;
-	if (n % 8 == 0)
-		copy_by_8(destcpy, srccpy, n);
-	else if (n % 7 == 0)
-		copy_by_7(destcpy, srccpy, n);
-	else if (n % 6 == 0)
-		copy_by_6(destcpy, srccpy, n);
-	else if (n % 5 == 0)
-		copy_by_5(destcpy, srccpy, n);
-	else if (n % 4 == 0)
-		copy_by_4(destcpy, srccpy, n);
-	else if (n % 3 == 0)
-		copy_by_3(destcpy, srccpy, n);
-	else if (n % 2 == 0)
-		copy_by_2(destcpy, srccpy, n);
-	else
-		while (n--)
-			*destcpy++ = *srccpy++;
+	while (n--)
+		*destcpy++ = *srccpy++;
 	return (dst);
 }
--- lib/libft/ft_memset.c
+++ lib/libft/ft_memset.c
@@ -3,41 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: ohakola+veilo <ohakola+veilo@student.hi    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 13:47:36 by ohakola           #+#    #+#             */
-/*   Updated: 2020/10/19 15:39:41 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/12/03 23:52:15 by ohakola+vei      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_utils.h"
-
-static void		set_by_3(unsigned char *dest, int c, size_t n)
-{
-	size_t		i;
-
-	i = 0;
-	while (i < n)
-	{
-		dest[i] = (unsigned char)c;
-		dest[i + 1] = (unsigned char)c;
-		dest[i + 2] = (unsigned char)c;
-		i += 3;
-	}
-}
-
-static void		set_by_2(unsigned char *dest, int c, size_t n)
-{
-	size_t		i;
-
-	i = 0;
-	while (i < n)
-	{
-		dest[i] = (unsigned char)c;
-		dest[i + 1] = (unsigned char)c;
-		i += 2;
-	}
-}
 
 /*
 ** The ft_memset() function writes len bytes of value c
@@ -49,24 +22,7 @@ void			*ft_memset(void *b, int c, size_t n)
 	unsigned char *cpy;
 
 	cpy = b;
-	if (b == NULL)
-		return (NULL);
-	if (n % 8 == 0)
-		set_by_8(cpy, c, n);
-	else if (n % 7 == 0)
-		set_by_7(cpy, c, n);
-	else if (n % 6 == 0)
-		set_by_6(cpy, c, n);
-	else if (n % 5 == 0)
-		set_by_5(cpy, c, n);
-	else if (n % 4 == 0)
-		set_by_4(cpy, c, n);
-	else if (n % 3 == 0)
-		set_by_3(cpy, c, n);
-	else if (n % 2 == 0)
-		set_by_2(cpy, c, n);
-	else
-		while (n--)
-			*cpy++ = (unsigned char)c;
+	while (n--)
+		*cpy++ = (unsigned char)c;
 	return (b);
 }
